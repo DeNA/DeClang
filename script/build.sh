b1c9530e6d57cc6d3bc614484c9d9efce3158a79
nevermoe <nevermoe>
2022-10-22 02:16:20 +0900

    add -DLLVM_ENABLE_RUNTIMES

--- script/build.sh
+++ script/build.sh
@@ -90,7 +90,8 @@ else
         -DLLVM_INCLUDE_EXAMPLES=OFF \
         -DLLVM_INCLUDE_TESTS=OFF \
         -DCMAKE_BUILD_TYPE=Release \
-        -DLLVM_ENABLE_PROJECTS="clang;libcxx" \
+        -DLLVM_ENABLE_PROJECTS="clang" \
+        -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi" \
         -DCMAKE_OSX_ARCHITECTURES="$build_arch" \
         -DLLVM_CCACHE_BUILD=${use_ccache}\
           -G "Unix Makefiles" ../llvm
