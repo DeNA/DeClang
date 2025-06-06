8dface7f5dd69f04aa4c72631483b5314d78c473
funa-tk <1781263+funa-tk@users.noreply.github.com>
2025-06-07 03:14:53 +0900

    remove unused logics from ndk_setup.sh

--- script/ndk_setup.sh
+++ script/ndk_setup.sh
@@ -61,11 +61,4 @@ if [[ ! -d "${darwin_path}"/lib.orig ]]; then
 fi
 cp -r "${compiler_path}"/lib/clang "${darwin_path}"/lib/
 
-src=`ls -d ${darwin_path}/lib64/clang/*/lib/linux/`
-echo $src
-for f in `ls ${darwin_path}/lib/clang/`; do
-  mkdir -p ${darwin_path}/lib/clang/$f/lib/
-  ln -s $src ${darwin_path}/lib/clang/$f/lib/ || true
-done
-
 popd >/dev/null
